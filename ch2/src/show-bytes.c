94153bef7735a9b6cc50152ede8a9601e1a566df
墨航 <ruohang.frh@alibaba-inc.com>
2016-12-01 10:16:50 +0800

    ch2 in progress

--- ch2/src/show-bytes.c
+++ ch2/src/show-bytes.c
@@ -1,105 +1,179 @@
-/* $begin show-bytes */
 #include <stdio.h>
 #include <string.h>
 typedef unsigned char *byte_pointer;
 
-void show_bytes(byte_pointer start, int len)
-{
+/* Show bytes in hex format */
+void show_bytes(byte_pointer start, int len) {
     int i;
-    for (i = 0; i < len; i++)
-	printf(" %.2x", start[i]);
-    printf("\n");
+    for (i = 0; i < len; i++){
+        printf(" %.2x", start[i]);
+    }
 }
 
-void show_int(int x)
-{
+/* Show single byte in bit format */
+void show_byte_in_bits(byte_pointer start) {
+    int i;
+    for (i = 0; i < 8 ; i++){
+        printf("%d", (*start & ( 1 << (i ^ 7) ))>0);
+    }
+}
+
+/* Show bytes in hex format (Turn little endian to big endian) */
+void show_bytes_be(byte_pointer start, int len) {
+    int i;
+    for (i = len -1 ; i >= 0; i--){
+        printf(" %.2x", start[i]);
+    }
+}
+
+/* Show bytes in bit format */
+void show_bits(byte_pointer start, int len) {
+    int i;
+    for (i = 0; i < len; i++){
+        show_byte_in_bits(start+i);
+        printf(" ");
+    }
+}
+
+/* Show bytes in bit format (Turn little endian to big endian) */
+void show_bits_be(byte_pointer start, int len) {
+    int i;
+    for (i = len -1 ; i >= 0; i--){
+        show_byte_in_bits(start+i);
+        printf(" ");
+    }
+}
+
+/* Show bit sequence of arbitrary length */
+void bits(byte_pointer start, int len) {
+    int i;
+    for (i = 0; i < len; i++){
+        if (i % 8 == 0)
+            printf(" ");
+        printf("%d",( *( start + (i >> 3) ) & ( 1 << (  (i & 7) ^ 7 ) ) ) > 0 );
+    }
+}
+
+
+void show_int(int x) {
+    printf("Integer value [decimal:%d] [hex:0x%x]\n",x,x);
+    printf("little endian:\t[");
     show_bytes((byte_pointer) &x, sizeof(int));
+    printf(" ]\t[");
+    show_bits((byte_pointer) &x, sizeof(int));
+    printf("]\n");
+
+    printf("big    endian:\t[");
+    show_bytes_be((byte_pointer) &x, sizeof(int));
+    printf(" ]\t[");
+    show_bits_be((byte_pointer) &x, sizeof(int));
+    printf("]");
 }
 
-void show_float(float x)
-{
+
+void show_float(float x) {
+    printf("Float value [decimal:%f]\n",x);
+    printf("little endian:\t[");
     show_bytes((byte_pointer) &x, sizeof(float));
+    printf(" ]\t[");
+    show_bits((byte_pointer) &x, sizeof(float));
+    printf("]\n");
+
+    printf("big    endian:\t[");
+    show_bytes_be((byte_pointer) &x, sizeof(float));
+    printf(" ]\t[");
+    show_bits_be((byte_pointer) &x, sizeof(float));
+    printf("]");
 }
 
-void show_pointer(void *x)
-{
+
+void show_pointer(void *x){
+    printf("Pointer value [decimal:%d] [hex: %x]\n",(int)(x), (int)(x));
+    printf("little endian:\t[");
     show_bytes((byte_pointer) &x, sizeof(void *));
+    printf(" ]\t[");
+    show_bits((byte_pointer) &x, sizeof(void *));
+    printf("]\n");
+
+    printf("big    endian:\t[");
+    show_bytes_be((byte_pointer) &x, sizeof(void *));
+    printf(" ]\t[");
+    show_bits_be((byte_pointer) &x, sizeof(void *));
+    printf("]");
 }
-/* $end show-bytes */
 
-
-/* $begin test-show-bytes */
-void test_show_bytes(int val)
-{
+void test_show_bytes(int val){
     int ival = val;
     float fval = (float) ival;
     int *pval = &ival;
+
     show_int(ival);
+    printf("\n");
+
     show_float(fval);
+    printf("\n");
+
     show_pointer(pval);
-}
-/* $end test-show-bytes */
-
-void simple_show()
-{
-/* $begin simple-show */
-int val = 0x12345678;
-byte_pointer valp = (byte_pointer) &val;
-show_bytes(valp, 1); /* A. */
-show_bytes(valp, 2); /* B. */
-show_bytes(valp, 3); /* C. */
-/* $end simple-show */
+    printf("\n");
 }
 
-void float_eg()
-{
-/* $begin float-show */
-int x = 3490593;
-float f = (float) x;
-show_int(x);
-show_float(f);
-/* $end float-show */
+
+
+void exercise2_5(){
+    int val = 0x87654321;
+    byte_pointer valp = (byte_pointer) &val;
+
+    printf("\n==============================================\n");
+    printf("Exercise 2.5: Show bytes [0~3] of hex %x. in small endian\n",val);
+
+    show_int(val);
+    printf("\n");
 }
 
-void string_eg()
-{
-/* $begin show-string */
-char *s = "ABCDEF";
-show_bytes((unsigned char *)s, strlen(s));
-/* $end show-string */
+void exercise2_6(){
+    int x = 3490593;
+    float f = (float) x;
+
+    int val = 0x87654321;
+    byte_pointer valp = (byte_pointer) &val;
+
+    printf("\n==============================================\n");
+    printf("Exercise 2.6: Show representation of integer %d and float %f\n", x, f);
+
+    show_int(x);
+    printf("\n");
+
+    show_float(f);
+    printf("\n");
+    printf("Hint: float.bits[9:] = int.bits[1:]\n");
 }
 
-void show_twocomp()
-{
-/* $begin show-twocomp */
-    short int x = 12345;
-    short int mx = -x;
 
-    show_bytes((byte_pointer) &x, sizeof(short int));
-    show_bytes((byte_pointer) &mx, sizeof(short int));
-/* $end show-twocomp */
+void exercise2_7(){
+    printf("\n==============================================\n");
+    printf("Exercise 2.7: show bytes on string: \n\t`show_bytes((byte_pointer)s,strlen(s));`\n");
+
+    const char *s = "abcdef";
+    show_bytes((byte_pointer)s,strlen(s));
+    printf("\n");
 }
 
-int main(int argc, char *argv[])
-{
-    int val = 12345;
-
+int main(int argc, char *argv[]) {
+    //gcc show-bytes.c -o show-bytes && ./show-bytes
+    int val;
     if (argc > 1) {
-      if (argv[1][0] == '0' && argv[1][1] == 'x')
-	sscanf(argv[1]+2, "%x", &val);
-      else
-	sscanf(argv[1], "%d", &val);
-      printf("calling test_show_bytes\n");
+      if (argv[1][0] == '0' && argv[1][1] == 'x'){
+        sscanf(argv[1]+2, "%x", &val);
+      } else {
+        sscanf(argv[1], "%d", &val);
+      }
+
+      printf("Reinterpret integer %d to int, float, pointer\n", val);
       test_show_bytes(val);
     } else {
-      printf("calling show_twocomp\n");
-      show_twocomp();
-      printf("Calling simple_show\n");
-      simple_show();
-      printf("Calling float_eg\n");
-      float_eg();
-      printf("Calling string_eg\n");
-      string_eg();
+      exercise2_5();
+      exercise2_6();
+      exercise2_7();
     }
     return 0;
 }
